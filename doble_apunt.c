852c309ee2ca1767b7f0bf7c2d56eb1ad7382804
eda1-fiunam <54489346+eda1-fiunam@users.noreply.github.com>
2024-02-18 16:34:51 -0600

    Cambia la numeración en las salidas

--- doble_apunt.c
+++ doble_apunt.c
@@ -3,15 +3,15 @@
 
 void f( float** p )
 {
-   printf( "(2) **p = %0.2f\n", **p );
-   printf( "(3)  *p = %p\n", *p );
-   printf( "(4)   p = %p; &p = %p\n", p, &p );
+   printf( "(3) **p = %0.2f\n", **p );
+   printf( "(4)  *p = %p\n", *p );
+   printf( "(5)   p = %p; &p = %p\n", p, &p );
 
    *p = NULL;
    // modificamos la dirección almacenada en p
 
-   printf( "(5)  *p = %p\n", *p );
-   printf( "(6)   p = %p; &p = %p\n", p, &p );
+   printf( "(6)  *p = %p\n", *p );
+   printf( "(7)   p = %p; &p = %p\n", p, &p );
 }
 
 int main()
@@ -20,16 +20,13 @@ int main()
 
    float* p_float = &val;
 
-   printf( "     val = %0.2f;               &val = %p\n", val, &val );
-   printf( " p_float = %p; &p_float = %p\n", p_float, &p_float );
-
-   printf( "(1) &val = %p\n", p_float );
-   // imprime la dirección de val
+   printf( "(1)     val = %0.2f;               &val = %p\n", val, &val );
+   printf( "(2) p_float = %p; &p_float = %p\n", p_float, &p_float );
 
    f( &p_float );
    // pasamos la dirección del apuntador
    
-   printf( "(7) &val = %p\n", p_float );
+   printf( "(8) &val = %p\n", p_float );
    // ¿Qué imprime?
 
    // ¿Qué sucede si descomentas y ejecutas la siguiente línea? Explica:
